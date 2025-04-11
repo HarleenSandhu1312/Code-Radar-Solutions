86e8df40961fe0cfa4a24f534014e9f838c99744
Harleen Kaur Sandhu <sandhuharleenkaur09@gmail.com>
2025-04-11 16:54:48 +0530

    Solution to Fibonacci Series

---
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -0,0 +1,11 @@
+void fibonacciSeries(int n){
+    if(n==0){
+        return 0;
+    }
+    if(n==1){
+        return 1;
+    }
+    else{
+        printf("%d",fibonacciSeries(n-1)+fibonacciSeries(n-2) )
+    }
+}
